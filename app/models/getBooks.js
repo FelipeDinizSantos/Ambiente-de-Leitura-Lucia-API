233c058f8a565ca6f197f015f9c20c80edbb061d
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-12-10 21:35:43 -0300

    Fixes

--- app/models/getBooks.js
+++ app/models/getBooks.js
@@ -4,7 +4,7 @@ const BookSchema = require('./bookSchema');
 async function getBooks(query) 
 {
     const [results] = await connection.execute(
-        "select l.id, l.titulo, l.autor, l.dataPublicacao as 'dataDePublicação', l.editora, c.nome as categoria, f.nome as 'faixaEtaria', l.sinopse, l.ISBN, l.numeroPaginas as 'numeroDePaginas', CASE WHEN ep.id_livro IS NOT NULL THEN 0 ELSE 1 END AS 'alugado' from tb_livro l inner join tb_faixaEtaria f on l.id_faixaEtaria = f.id inner join tb_livroCategoria lc on l.id = lc.id_livro inner join tb_categoria c on lc.id_categoria = c.id left join tb_emprestimoLivro ep on ep.id_livro = l.id where l.titulo like ? or l.id like ? or l.titulo like ?;",
+        "select l.id, l.titulo, l.autor, l.dataPublicacao as 'dataDePublicação', l.editora, c.nome as categoria, f.nome as 'faixaEtaria', l.sinopse, l.ISBN, l.numeroPaginas as 'numeroDePaginas', CASE WHEN ep.id_livro IS NOT NULL THEN 1 ELSE 0 END AS 'alugado' from tb_livro l inner join tb_faixaEtaria f on l.id_faixaEtaria = f.id inner join tb_livroCategoria lc on l.id = lc.id_livro inner join tb_categoria c on lc.id_categoria = c.id left join tb_emprestimoLivro ep on ep.id_livro = l.id where l.titulo like ? or l.id like ? or l.titulo like ?;",
         [`${query}%`, `${query}%`, `%${query}%`,]
     );
 
